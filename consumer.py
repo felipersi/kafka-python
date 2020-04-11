a7df4ea6359e912f171f91b0fb56e1a1358dce1a
Tiago Krebs <55988677+tiagorkrebs@users.noreply.github.com>
2020-04-11 15:24:52 -0300

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -1,8 +1,7 @@
 from kafka import KafkaConsumer
 from json import loads
 
-consumer = KafkaConsumer('numtest', bootstrap_servers=['kfk-bhs-ovh001s.infra.azion.net:9092',
-                                                       'kfk-bhs-ovh002s.infra.azion.net:9092'],
+consumer = KafkaConsumer('numtest', bootstrap_servers=['host:9092'],
                          auto_offset_reset='earliest', enable_auto_commit=True, group_id='kafka-python-group',
                          value_deserializer=lambda x: loads(x.decode('utf-8')),
                          api_version=(0, 9))
