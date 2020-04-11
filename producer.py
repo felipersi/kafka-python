c871dcd45daff170e239c55690b499493c8d52f6
Tiago Krebs <55988677+tiagorkrebs@users.noreply.github.com>
2020-04-11 15:25:12 -0300

    Update producer.py

--- producer.py
+++ producer.py
@@ -2,8 +2,7 @@ from time import sleep
 from json import dumps
 from kafka import KafkaProducer
 
-producer = KafkaProducer(bootstrap_servers=['kfk-bhs-ovh001s.infra.azion.net:9092',
-                                            'kfk-bhs-ovh002s.infra.azion.net:9092'],
+producer = KafkaProducer(bootstrap_servers=['host:9092'],
                          value_serializer=lambda x: dumps(x).encode('utf-8'))
 
 for e in range(1000):
